78b33a7f603f3f6be9faa401783830edf3016299
Ozgun137 <ozgunozkan95@gmail.com>
2025-01-09 22:36:32 -0500

    Meme list action

---
+++ app/src/main/java/com/example/mastermeme/presentation/memeList/MemeListAction.kt
@@ -0,0 +1,6 @@
+package com.example.mastermeme.presentation.memeList
+
+sealed interface MemeListAction {
+    data object OnCreateMemeClicked : MemeListAction
+    data object OnBottomSheetDismissed : MemeListAction
+}
